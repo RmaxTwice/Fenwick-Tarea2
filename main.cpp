98183ea5b91d8022d36d2c5f9717731bcf5a6121
Raquel Escalante <escalante.raquelj@gmail.com>
2017-10-05 22:21:49 -0400

    Add extra functions for FenTree updating (not complete)
    -New functions: Update_SingleTree, add_neg, remove_neg
    -Change initialization value for FenTrees to 1 in constructor
    -arr_xi temporarily set to all ones and update function set to a negative number

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 Elaborado por: Raquel Escalante y Rafael Vasquez
 */
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 ////////////////////// Definicion de Operaciones de FenTree ///////////////////////////
 int FenTree_Sum(int FenTree[], int idx)
 {
-    int sum_neg = 0; //Almacena la suma de negativos en el tree
+    int sum_neg = 0;
     idx = idx + 1;
 
     while (idx > 0)
@@ -17,27 +18,76 @@ int FenTree_Sum(int FenTree[], int idx)
     }
     return sum_neg;
 }
+/////////////////////////////////////Funciones de Update///////////////////////////////////////
+int add_neg(int idx, int FenTree[])
+{
+    int sum;
+    if (FenTree[idx]<(-1) || FenTree[idx] > 1){
+        sum = abs(FenTree[idx])+1;
+    }else{
+        ;
+    }
+    return sum;
+}
+
+int remove_neg(int idx, int FenTree[])
+{
+    int sum;
+    if (FenTree[idx]==1)
+    {
+        sum = FenTree[idx] = -abs(FenTree[idx]);
+    }
+    else if (FenTree[idx]>1)
+    {
+        sum = FenTree[idx] = (abs(FenTree[idx])-1)*-1;
+    }
+    return sum;
+}
+
+void Update_SingleTree(int val, int idx, int FenTree[])
+{
+    if (val > 0)
+    {
+            ; //pass
+    }
+    else
+    {
+        if (val < 0)
+        {
+            FenTree[idx] = add_neg(idx, FenTree);
+        }
+        else if (val==0)
+        {
+            FenTree[idx] = remove_neg(idx, FenTree);
+        }
+    }
+}
 
 void FenTree_Update(int FenTree[], int n, int idx, int val)
 {
     idx = idx + 1;
     while (idx <= n)
     {
-        //Funcion actualizada:
-
-        //FenTree[idx] += val;
+        Update_SingleTree(val, idx, FenTree);
         idx += idx & (-idx);
     }
-    //for (int i=1; i<=n; i++) cout << FenTree[i] << " ";
+    cout << endl;
+    for (int i=1; i<=n; i++) cout << FenTree[i] << " ";
     cout << endl;
 }
+//////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////Funcion de cambio de valor/////////////////////////////////
+
 
 /*void change_xi(int a_xi[], int idx, int val)
 {
     a_xi[idx] = val;
 }
 */
+//////////////////////////////////////////////////////////////////////////////////////////
 
+//////////////////////////////Funcion de rango////////////////////////////////////////////
 int FenTree_Range(int FenTree[], int idx1, int idx2)
 {
     int sum_neg = 0;
@@ -45,13 +95,15 @@ int FenTree_Range(int FenTree[], int idx1, int idx2)
     sum_neg -= FenTree_Sum(FenTree, idx1);
     return sum_neg;
 }
+//////////////////////////////////////////////////////////////////////////////////////////
 
+////////////////////////////////Constructor///////////////////////////////////////////////
 int *FenTree_constructor(int a_xi[], int n)
 {
     int *FenTree = new int[n+1];
     for (int i=1 ; i<=n ; i++)
     {
-        FenTree[i] = 0;
+        FenTree[i] = 1;                 //Se cambia el modo de inicialización
         cout << FenTree[i] << " ";
     }
     for (int i=0; i<n; i++)
@@ -88,7 +140,7 @@ int main()
     }
     cout << endl;
     */
-    int arr_xi[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int arr_xi[] = {1, 1, 1, 1, 1, 1, 1, 1};
     int n = sizeof(arr_xi)/sizeof(arr_xi[0]);
 
     ////////////////////// Inicializacion de el arbol de fenwick /////////////////////////
@@ -116,8 +168,8 @@ int main()
         cout << "\nSum of elements in arr[0..i] is "<< FenTree_Sum(FTree, i);
 
     // Let use test the update operation
-    arr_xi[3] += 6;
-    FenTree_Update(FTree, n, 3, 6); //Update BIT for above change in arr[]
+    arr_xi[3] += -6;
+    FenTree_Update(FTree, n, 3, -6); //Update BIT for above change in arr[]
 
     for(int i=0; i<n; i++)
         cout << "\nSum of elements in arr[0..i] after update is "<< FenTree_Sum(FTree, i);
