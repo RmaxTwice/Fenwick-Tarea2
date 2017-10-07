29b3c7ce71947f87d18d3b9f0408b5e7fd6fa421
RmaxTwice <ravb2013@gmail.com>
2017-10-07 00:27:03 -0400

    Assignment almost complete, dynamic mem missing.
    - New structure to count negative numbers and zeroes.
    - Complete overhaul of the update and sum functions.
    - Added 2 operational functions (API like) for valueChange
      and RangeQuery.
    - Stdin and stdout integrated to take and give data.
    - Missing support for dynamic arrays.

--- main.cpp
+++ main.cpp
@@ -2,123 +2,198 @@
 Elaborado por: Raquel Escalante y Rafael Vasquez
 */
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-////////////////////// Definicion de Operaciones de FenTree ///////////////////////////
-int FenTree_Sum(int FenTree[], int idx)
+class BITreeValue{
+public:
+    int negatives;
+    int zeroes;
+};
+
+///////////////////////////////// Funciones de Consulta //////////////////////////////////////
+BITreeValue FenTree_Sum(BITreeValue FenTree[], int idx)
 {
-    int sum_neg = 0; //Almacena la suma de negativos en el tree
+    BITreeValue sum;
+    sum.negatives = 0;
+    sum.zeroes = 0;
     idx = idx + 1;
 
     while (idx > 0)
     {
-        sum_neg += FenTree[idx];
+        sum.negatives += FenTree[idx].negatives;
+        sum.zeroes += FenTree[idx].zeroes;
         idx -= idx & (-idx);
     }
-    return sum_neg;
+    return sum;
 }
 
-void FenTree_Update(int FenTree[], int n, int idx, int val)
+BITreeValue FenTree_Range(BITreeValue FenTree[],int val_arr[], int idx1, int idx2)
 {
+    BITreeValue sum, aux;
+
+    sum = FenTree_Sum(FenTree, idx2);
+    aux = FenTree_Sum(FenTree, idx1);
+    sum.negatives -= aux.negatives;
+    sum.zeroes -= aux.zeroes;
+    if(val_arr[idx1] < 0) sum.negatives++;
+    if(val_arr[idx1] == 0) sum.zeroes++;
+
+    return sum;
+}
+///////////////////////////////////// Funciones de Update ///////////////////////////////////////
+
+
+BITreeValue calculate_difference(int val, int idx, BITreeValue FenTree[], int freq[])
+{
+    BITreeValue result;
+    result.zeroes = 0;
+    result.negatives = 0;
+
+    if( freq[idx - 1] == 0){// Si el valor viejo es igual a cero...
+        if(val == 0){       // ...y el valor nuevo es cero: No aumentamos ningun contador.
+            result.zeroes = 0;
+            result.negatives = 0;
+        }else{
+            if(val > 0){    // ...y el valor nuevo es positivo: restamos uno al contador de ceros.
+                result.zeroes = -1;
+                result.negatives = 0;
+            }else{          // ...y el valor nuevo es negativo: invertimos el signo y contamos un negativo.
+                result.zeroes = -1;
+                result.negatives = 1;
+            }
+        }
+    }else{
+        if(freq[idx - 1] > 0){      //Si el valor viejo es positivo...
+            if(val == 0){           // ...y el valor nuevo es cero: aumentamos el contador de ceros.
+                result.zeroes = 1;
+                result.negatives = 0;
+            }else{
+                if(val > 0){        // ...y el valor nuevo es positivo: No aumentamos ningun contador.
+                    result.zeroes = 0;
+                    result.negatives = 0;
+                }else{              // ...y el valor nuevo es negativo: contamos un negativo.
+                    result.zeroes = 0;
+                    result.negatives = 1;
+                }
+            }
+        }else{                      //Si el valor viejo es negativo...
+            if(val == 0){           // ...y el valor nuevo es cero: restamos un negativo e aumentamos un zero.
+                result.zeroes = 1;
+                result.negatives = -1;
+            }else{
+                if(val > 0){        // ...y el valor nuevo es positivo: restamos un negativo.
+                    result.zeroes = 0;
+                    result.negatives = -1;
+                }else{              // ...y el valor nuevo es negativo: No aumentamos ningun contador.
+                    result.zeroes = 0;
+                    result.negatives = 0;
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+
+void FenTree_Update(BITreeValue FenTree[], int n, int idx, int val, int val_arr[])
+{
+    BITreeValue diff;
+
     idx = idx + 1;
+    diff = calculate_difference(val, idx, FenTree, val_arr);
+
     while (idx <= n)
     {
-        FenTree[idx] += val;
+        FenTree[idx].negatives += diff.negatives;
+        FenTree[idx].zeroes += diff.zeroes;
         idx += idx & (-idx);
     }
-    //for (int i=1; i<=n; i++) cout << FenTree[i] << " ";
-    cout << endl;
 }
+///////////////////////////////////////////////////////////////////////////////////////////////
 
-/*void change_xi(int a_xi[], int idx, int val)
+////////////////////////////////Constructor///////////////////////////////////////////////
+BITreeValue *FenTree_constructor(int a_xi[], int n)
 {
-    a_xi[idx] = val;
-}
-*/
-
-int FenTree_Range(int FenTree[], int idx1, int idx2)
-{
-    int sum_neg = 0;
-    sum_neg = FenTree_Sum(FenTree, idx2);
-    sum_neg -= FenTree_Sum(FenTree, idx1);
-    return sum_neg;
-}
-
-int *FenTree_constructor(int a_xi[], int n)
-{
-    int *FenTree = new int[n+1];
+    BITreeValue *FenTree = new BITreeValue[n+1];
     for (int i=1 ; i<=n ; i++)
-    {
-        FenTree[i] = 0;
-        cout << FenTree[i] << " ";
+    {   // Inicializamos todos los valores del BIT en 0.
+        FenTree[i].negatives = 0;
+        FenTree[i].zeroes = 0;
     }
-    for (int i=0; i<n; i++)
-        FenTree_Update(FenTree, n, i, a_xi[i]);
 
-    cout << endl;
+    for (int i=0; i<n; i++) FenTree_Update(FenTree, n, i, a_xi[i],a_xi);
+
     return FenTree;
 }
+///////////////////////////// Funciones de Operaciones ////////////////////////////////////////
+void changeValue(int val_arr[], BITreeValue FenTree[], int n, int index, int val){
+
+    FenTree_Update(FenTree, n, index-1, val, val_arr);
+    val_arr[index-1] = val;
+   /*cout <<"Updt: ";
+    for (int i=1; i<=n; i++) cout <<" (" <<FenTree[i].negatives << ", "<<FenTree[i].zeroes << ")";
+    cout << "\n\n" ;*/
+}
+
+char querySignRange(int val_arr[], BITreeValue FenTree[], int idx1, int idx2){
+    BITreeValue result = FenTree_Range(FenTree, val_arr, idx1-1, idx2-1);
+    if (result.zeroes != 0) return '0';
+    if (result.negatives % 2) return '-';
+    return '+';
+}
 
 
- //////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
     int N, m, i;
     char op;
     int indx1, indx2, val;
+    BITreeValue result;
     ////////////////////// Lecturas de variables iniciales ///////////////////////////////
     cin >> N;
-    cout << "Tamanio del arreglo: " << N <<endl;
+    //cout << "Tamanio del arreglo: " << N <<endl;
 
     cin >> m;
-    cout << "Numero de operaciones: " << m <<endl;
+    //cout << "Numero de operaciones: " << m <<endl;
     cin.ignore();   // Elimina el \n del final despues de leer m
     //////////////////////////////////////////////////////////////////////////////////////
-    ///////////////////////// Declaracion del arreglo de Xi //////////////////////////////
-    /*int* arr_xi;
-    arr_xi = new int[N];
 
-    for (i = 0; i < N ; i++)
-    {
-        arr_xi[i] = 1;
-        cout << arr_xi[i] << " ";
-    }
-    cout << endl;
-    */
-    int arr_xi[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    ///////////////////////// Declaracion del arreglo de Xi //////////////////////////////
+
+    int arr_xi[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
     int n = sizeof(arr_xi)/sizeof(arr_xi[0]);
 
     ////////////////////// Inicializacion de el arbol de fenwick /////////////////////////
-    int *FTree = FenTree_constructor(arr_xi, n);
+    BITreeValue *FTree = FenTree_constructor(arr_xi, n);
     //////////////////////////////////////////////////////////////////////////////////////
-    //int rango;
+
     ////////////////////// Operaciones sobre el arbol de fenwick /////////////////////////
-    /*for( i = 0; i < m; i++ ){
+    for( i = 0; i < m; i++ ){
         cin >> op;
         if(op == 'c'){
             cin >> indx1 >> val ;
-            cout << "Cambio de valor: " << indx1 << ", " << val << endl;
-            change_xi(arr_xi, indx1,val);
-            FenTree_Update(FTree, N, indx1, val);
+            //cout << "Cambio de valor: " << indx1 << ", " << val << endl;
+            changeValue(arr_xi, FTree, n, indx1, val);
         }else{
             cin >> indx1 >> indx2 ;
-            cout << "Consulta: " << indx1 << ", " << indx2 << endl;
-            rango = FenTree_Range(FTree, indx1, indx2);
-            cout << rango << endl;
+            //cout << "Consulta: " << indx1 << ", " << indx2 << endl;
+            cout << querySignRange(arr_xi, FTree, indx1, indx2) <<"\n";
         }
     }
-    */
+
     //////////////////////////////////////////////////////////////////////////////////////
-    for(i=0; i<n; i++)
-        cout << "\nSum of elements in arr[0..i] is "<< FenTree_Sum(FTree, i);
 
-    // Let use test the update operation
-    arr_xi[3] += 6;
-    FenTree_Update(FTree, n, 3, 6); //Update BIT for above change in arr[]
+    /*
+    changeValue(arr_xi, FTree, n, 1, 7);
+    changeValue(arr_xi, FTree, n, 2, -4);
+    cout << querySignRange(arr_xi, FTree, 1, 2) <<"\n";
+    cout << querySignRange(arr_xi, FTree, 3, 5) <<"\n";
+    */
 
-    for(int i=0; i<n; i++)
-        cout << "\nSum of elements in arr[0..i] after update is "<< FenTree_Sum(FTree, i);
-
+    delete[] FTree;
     return 0;
 }
